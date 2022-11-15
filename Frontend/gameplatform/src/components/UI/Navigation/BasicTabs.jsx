032d312b5c6c23f42a04026853f98413ef7581ab
kizzl01 <Lukas.Michalek@hotmail.de>
2022-11-15 15:52:33 +0100

    Frontend User Eingabemaske

--- Frontend/gameplatform/src/components/UI/Navigation/BasicTabs.jsx
+++ Frontend/gameplatform/src/components/UI/Navigation/BasicTabs.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
+import { StyledEngineProvider } from "@mui/material";
+import "./BasicTabs.css";
 
 function BasicTabs(props) {
   const [value, setValue] = useState(0);
@@ -11,22 +13,26 @@ function BasicTabs(props) {
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={(e,v)=>{props.onClick(v);handleChange(e,v)}}
-          aria-label="basic tabs example"
-          orientation="vertical"
-          visibleScrollbar={false}
-
-        >
-          <Tab label="Tic Tac Toe" />
-          <Tab label="Snake" />
-          <Tab label="Game 3" />
-        </Tabs>
+    <StyledEngineProvider>
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={(e, v) => {
+              props.onClick(v);
+              handleChange(e, v);
+            }}
+            aria-label="basic tabs example"
+            orientation="vertical"
+            visibleScrollbar={false}
+          >
+            <Tab label="Tic Tac Toe" />
+            <Tab label="Snake" />
+            <Tab label="Game 3" />
+          </Tabs>
+        </Box>
       </Box>
-    </Box>
+    </StyledEngineProvider>
   );
 }
 
--- Frontend/gameplatform/src/components/UI/UIMain.jsx
+++ Frontend/gameplatform/src/components/UI/UIMain.jsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import Snake from "../Games/Snake/snake.js";
 import GameTicTacToe from "../Games/TicTacToe/GameTicTacToe";
@@ -5,13 +6,46 @@ import ScoreBoard from "./Information/ScoreBoard.jsx";
 import BasicTabs from "./Navigation/BasicTabs";
 import "./UIMain.css";
 
+function UserInput(props) {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div className="user-input">
+      <TextField
+        id="username"
+        label="Nutzername"
+        variant="filled"
+        onChange={handleChange}
+        value={text}
+      />
+      <Button variant="contained" onClick={() => props.onAccept(text)}>
+        Bestätigen
+      </Button>
+    </div>
+  );
+}
+
 function Main() {
-  const [user, setUser] = useState("Lukas");
+  const [user, setUser] = useState(null);
   const [contentItem, setContentItem] = useState(0);
   const [HighScores, setHighScores] = useState(null);
+  const [userList, setUserlist] = useState(null);
 
   // Kommunikation Server -> Client
   // methode zum daten anfragen per httpget methode. spricht den endpunkt der API an der in der fetch methode angegeben ist
+  function compareScores(a, b) {
+    if (a.score < b.score) {
+      return 1;
+    }
+    if (a.score > b.score) {
+      return -1;
+    }
+    return 0;
+  }
 
   const getScoreBoard = () => {
     fetch("https://localhost:44384/api/SnakeScoreBoard", {
@@ -21,15 +55,30 @@ function Main() {
     })
       .then((e) => e.json())
       .then((e) => {
+        e = e.sort(compareScores);
         setHighScores(e);
       });
     return null;
   };
 
+  const getUserList = () => {
+    fetch("https://localhost:44384/api/User", {
+      crossDomain: true,
+      method: "GET",
+      headers: { "Content-type": "application/json" },
+    })
+      .then((e) => e.json())
+      .then((e) => {
+        setUserlist(e);
+      });
+    return null;
+  }
+
   // Kommunikation Client -> Server
   // methode zum posten per httppost. spricht den Endpunkt der API an der in der fetch methode angegeben ist
 
   const postScoreBoard = (data) => {
+    data = data.sort(compareScores);
     const requestOptions = {
       crossDomain: true,
       method: "POST",
@@ -44,12 +93,29 @@ function Main() {
   // ist nötig, da die UIMain komponente function based ist und somit keinen konstruktor hat
 
   useEffect(() => {
+    getUserList();
     getScoreBoard();
+    // window.addEventListener("beforeunload", alertuser);
+    // return () => {
+    //   window.removeEventListener("beforeunload", alertuser);
+    // };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const alertuser = (e) => {
+    e.preventDefault();
+    e.returnValue = "";
+  };
 
   const handleClick = (i) => {
     setContentItem(i);
+    console.log("UserListe: ", userList);
   };
+
+  const handleUserLogon = (u) =>{
+    
+    setUser(u);
+  }
 
   // Die Methode gibt als Rückgabewert ein Array von Komponenten zurück. Hier werden die einzelnen Spiele später eingefügt!
 
@@ -61,34 +127,55 @@ function Main() {
     return c;
   };
 
-  const WriteNewHighScores = (n, s) => {
-    const copy = HighScores.slice();
-    console.log("old: ", HighScores);
-    if (copy[copy.findIndex((x) => x.name === n)].score < s) {
-      copy[copy.findIndex((x) => x.name === n)].score = s;
-      setHighScores(copy);
-      postScoreBoard(copy);
+  const ExistingUser = (data, n) => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].name.toLowerCase() === n) return true;
     }
+    return false;
+  };
+
+  const WriteNewHighScores = (user, s) => {
+    const n = user.toLowerCase();
+    // const copy = JSON.parse(JSON.stringify(HighScores)); //erstellt eine Echte Kopie eines JSON objektes
+    let copy = HighScores.slice(); // erstellt nur eine referenz auf das echte objekt (shallow Copy)
+    if (!ExistingUser(copy, n)) {
+      HighScores.push({ name: user, score: s });
+      copy = HighScores.slice();
+    } else if (
+      copy[copy.findIndex((x) => x.name.toLowerCase() === n)].score < s
+    ) {
+      copy[copy.findIndex((x) => x.name.toLowerCase() === n)].score = s;
+    }
+    copy = copy.sort(compareScores);
+    setHighScores(copy);
+    postScoreBoard(copy);
   };
 
   const content = contents()[contentItem];
   return (
-    <div className="main-wrapper">
-      <div className="navigation-container">
-        <BasicTabs onClick={(i) => handleClick(i)} />
-      </div>
-      {content !== null && (
-        <div className="content-container">
-          <div className="game-wrapper">
-            <div className="game-container">{content}</div>
+    <div className="root-wrapper" style={{ height: "100%", width: "100%" }}>
+      {!user && <UserInput onAccept={handleUserLogon} />}
+      {user && content !== null && (
+        <div className="main-wrapper">
+          <div className="navigation-container">
+            <BasicTabs onClick={(i) => handleClick(i)} />
+          </div>
+          <div className="content-container">
+            <div className="game-wrapper">
+              <div className="game-container">{content}</div>
+            </div>
+          </div>
+          <div className="info-container">
+            {content && content.type.name === "Snake" && (
+              <ScoreBoard
+                data={HighScores}
+                getData={getScoreBoard}
+                sortFunction={compareScores}
+              />
+            )}
           </div>
         </div>
       )}
-      <div className="info-container">
-        {content && content.type.name === "Snake" && (
-          <ScoreBoard data={HighScores} getData={getScoreBoard} />
-        )}
-      </div>
     </div>
   );
 }
